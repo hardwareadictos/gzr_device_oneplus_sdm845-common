9cdb823395b5bfe4e77f0605ee87293bba7d0f4b
hardwareadictos <hardwareadictos@gmail.com>
2018-07-18 14:40:47 +0200

    sdm845-common: Bring and build OnePlus Doze
    Change-Id: I6347ebc37938cf195aad53b04116fe3778539ee1

--- common.mk
+++ common.mk
@@ -70,6 +70,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libvulkan
 
+# Doze mode
+PRODUCT_PACKAGES += \
+    OneplusDoze    
+
 # HotwordEnrollement app permissions
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/privapp-permissions-hotword.xml:system/etc/permissions/privapp-permissions-hotword.xml
