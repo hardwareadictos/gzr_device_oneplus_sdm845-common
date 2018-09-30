97e8ce7b2ab5e7b353d5b76faa80d5aa0b855dd2
hardwareadictos <hardwareadictos@gmail.com>
2018-09-30 14:52:04 +0200

    sdm845-common: temp: ignore SEPolicy neverallows

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -123,6 +123,7 @@ BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(COMMON_PATH)/sepolicy/private
 
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += device/qcom/sepolicy/private
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR += device/qcom/sepolicy/public
+SELINUX_IGNORE_NEVERALLOWS := true
 
 # Verified Boot
 BOARD_BUILD_DISABLED_VBMETAIMAGE := true
