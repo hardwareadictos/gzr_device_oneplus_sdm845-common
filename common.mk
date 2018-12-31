d7b2616734e5174e5c5e2367e294a26e544a032b
hardwareadictos <hardwareadictos@gmail.com>
2018-12-31 19:02:31 +0100

    sdm845-common: Delete Custom AmbientDisplay

--- common.mk
+++ common.mk
@@ -19,8 +19,7 @@ $(call inherit-product, vendor/oneplus/sdm845-common/sdm845-common-vendor.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay 
 
 # Properties
 -include $(LOCAL_PATH)/system_prop.mk
@@ -70,10 +69,6 @@ PRODUCT_PACKAGES += \
 # Display
 PRODUCT_PACKAGES += \
     libvulkan
-
-# Doze
-PRODUCT_PACKAGES += \
-    OnePlusDoze
 
 # HotwordEnrollement app permissions
 PRODUCT_COPY_FILES += \
