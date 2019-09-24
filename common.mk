2eb44548b7ee5914330d7a63fbe9dfd280cafe70
hardwareadictos <hardwareadictos@gmail.com>
2019-09-24 19:54:43 +0200

    sdm845-common: disable nfc components BLOBs COPY SHOULD BE DISABLED ON VENDOR ALSO

--- common.mk
+++ common.mk
@@ -101,7 +101,7 @@ PRODUCT_PACKAGES += \
     netutils-wrapper-1.0
 
 # NFC
-PRODUCT_PACKAGES += \
+#PRODUCT_PACKAGES += \
     android.hardware.nfc@1.0:64 \
     android.hardware.nfc@1.1:64 \
     android.hardware.secure_element@1.0:64 \
