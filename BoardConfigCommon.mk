2b4e071b8c41df7d6081ef1b1fcd02b42ee99bea
hardwareadictos <hardwareadictos@gmail.com>
2019-10-18 13:45:36 +0200

    sdm845-common: build qcom sepolicy

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -123,11 +123,11 @@ TARGET_PROVIDES_QTI_TELEPHONY_JAR := true
 # Sepolicy
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(COMMON_PATH)/sepolicy/private
 
-#BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
     device/qcom/sepolicy/generic/private \
     device/qcom/sepolicy/qva/private
 
-#BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
     device/qcom/sepolicy/generic/public \
     device/qcom/sepolicy/qva/public
 
